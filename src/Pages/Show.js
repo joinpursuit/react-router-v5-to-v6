ed5d4db897fc976e93cbb2fd4b386c882cf4c38b
Karolin Rafalski <kr2174@caa.columbia.edu>
2021-12-13 15:57:55 -0500

    fix typo

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,12 +1,13 @@
-import { Link, Route } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 
 function Show() {
+  const { id } = useParams();
   return (
     <div>
       <h2>Show Page</h2>
       <h3>This plant needs medium light and infrequent watering.</h3>
       <Link to={`/plants/more-details`}>See more details about this plant</Link>
-      <Route path="/plants/more-details">
+      <Route path={`/plants/more-details`}>
         <p>This plant is very cool</p>
       </Route>
     </div>
